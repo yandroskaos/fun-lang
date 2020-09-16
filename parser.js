f9610f2008abd56a3d903df65bf984de59c8db15
Pablo Bravo <yandroskaos@gmail.com>
2020-09-16 18:18:04 +0200

    parser linted and allows naming composite patterns, 'rest' moved out of composite

--- parser.js
+++ parser.js
@@ -27,49 +27,51 @@ const compose = (...fns) => initial => {
     return r
 }
 
-function toBool(str) {
-    return str == "true"
-}
+const parseBool = str => str == "true"
 
-function pruneString(str) {
-    return str.slice(1, str.length-1);
-}
+const parseString = str => str.slice(1, str.length-1)
 
-function prune(list) {
-    return list.length == 1 ? list[0] : list;
-}
+const prune = list => list.length == 1 ? list[0] : list
 
 const merge = type => list => {
-    const hasName = list[0].type != undefined
-    const hasRest = list[list.length - 1].type != undefined
+    const [a, b, c] = list
 
-    let body = hasName ? list[1] : list[0]
+    const hasName = !Array.isArray(a)
+
+    let name = hasName ? a.data : undefined
+    let body = hasName ? b : a
+    let rest = hasName ? c : b
+
+    const hasRest = rest != undefined
 
     if(hasRest)
-        body.push(list[list.length - 1])
+        body.push(rest)
 
-    if(hasName)
-        return {name: list[0].data, type, data:body}
-
-    return {type, data:body}
+    return hasName ? {name, type, data:body} : {type, data:body}
 }
 
-function toField(list) {
-    return {
-        name: list[0], 
-        data: list.length == 1 ? { name: 'placeholder', data: '_' } : list[1]
-    };
-}
+const toField = list => {
+    let [name, value] = list
 
-function toFunctionDefinition(list) {
-    return {
-        name: list[1],
-        arguments: list[0],
-        body: list[2],
+    if (value) {
+        value = Array.isArray(value) ? prune(value) : value
+    } else {
+        value = { type: 'any' }
     }
+
+    return {name, ...value}
 }
 
-const toRule = type => data => ({type, data})
+const toFunction = list => {
+    const [pattern, name, body] = list
+    return {name, pattern, body}
+}
+
+const toExpr = type => data => ({type, data})
+
+
+
+
 
 const operator          = rule.make()
 
@@ -92,11 +94,9 @@ const pattern_field        = rule.make()
 const pattern_object       = rule.make()
 const pattern              = rule.make()
 
-const function_argument    = rule.make()
-const function_definition  = rule.make()
+const function_pattern     = rule.make()
 const function_body        = rule.make()
-const symbol               = rule.make()
-
+const function_definition  = rule.make()
 
 const program              = rule.make()
 
@@ -107,40 +107,35 @@ rule.set(operator, choice(
     match("&&"), match("||"), match("!"),
     match("+"), match("-"), match("*"), match("/"), match("%")),
     
-    toRule("operator") );
+    toExpr("operator") );
 
 
 const aggregate_expr = (open, body, close) => sequence(ignore(open), star(body), ignore(close))
 
-rule.set(expr_symbol, match("ID"),                                           toRule("symbol")                       );
-rule.set(expr_bool,   match("BOOL"),                                         compose(toBool, toRule("bool"))        );
-rule.set(expr_number, match("NUMBER"),                                       compose(parseInt, toRule("number"))    );
-rule.set(expr_string, match("STRING"),                                       compose(pruneString, toRule("string")) );
+rule.set(expr_symbol, match("ID"),                                           toExpr("symbol")                       );
+rule.set(expr_bool,   match("BOOL"),                                         compose(parseBool, toExpr("bool"))     );
+rule.set(expr_number, match("NUMBER"),                                       compose(parseInt, toExpr("number"))    );
+rule.set(expr_string, match("STRING"),                                       compose(parseString, toExpr("string")) );
 rule.set(expr_basic,  choice(expr_bool, expr_number, expr_string, expr_symbol, operator)                            );
-rule.set(expr_list,   aggregate_expr("[", expr,"]"),                         compose(prune, toRule("list"))         );
+rule.set(expr_list,   aggregate_expr("[", expr,"]"),                         compose(prune, toExpr("list"))         );
 rule.set(expr_field,  sequence(match("ID"), ignore(":"), expr),              toField                                );
-rule.set(expr_object, aggregate_expr("{", expr_field,"}"),                   compose(prune, toRule("object"))       );
+rule.set(expr_object, aggregate_expr("{", expr_field,"}"),                   compose(prune, toExpr("object"))       );
 rule.set(expr,        choice(expr_basic, expr_list, expr_object)                                                    );
 
 const aggregate_pattern = (open, body, close) => sequence(optional(pattern_name), ignore(open), star(body), ignore(close), optional(pattern_rest))
 
-rule.set(pattern_placeholder,  match("_"),                             toRule("placeholder")          );
-rule.set(pattern_basic,        choice(pattern_placeholder, expr_basic)                                );
-rule.set(pattern_name,         sequence(match("ID"), ignore("@")),     compose(prune, toRule("name")) );
-rule.set(pattern_rest,         sequence(ignore("::"), match("ID")),    compose(prune, toRule("rest")) );
-rule.set(pattern_list,         aggregate_pattern("[", pattern, "]"),   merge("list") );
-rule.set(pattern_field,        choice(
-                                   sequence(choice(match("ID"), match("_")), ignore(":"), pattern),
-                                   sequence(match("ID"))),
-                               toField                                                                );
-rule.set(pattern_object,       aggregate_pattern("{", pattern_field, "}"), merge("object")            );
-rule.set(pattern,              choice(pattern_list, pattern_object, pattern_basic)                    );
+rule.set(pattern_placeholder,  match("_"),                                                                          () => ({type:"any"})           );
+rule.set(pattern_basic,        choice(pattern_placeholder, expr_basic)                                                                             );
+rule.set(pattern_name,         sequence(match("ID"), ignore("@")),                                                  compose(prune, toExpr("name")) );
+rule.set(pattern_rest,         sequence(ignore("::"), match("ID")),                                                 compose(prune, toExpr("rest")) );
+rule.set(pattern_list,         aggregate_pattern("[", pattern, "]"),                                                merge("list")                  );
+rule.set(pattern_field,        sequence(choice(match("ID"), match("_")), optional(sequence(ignore(":"), pattern))), toField                        );
+rule.set(pattern_object,       aggregate_pattern("{", pattern_field, "}"),                                          merge("object")                );
+rule.set(pattern,              choice(pattern_list, pattern_object, pattern_basic)                                                                 );
 
-
-rule.set(function_argument,    sequence(notAt(sequence(match("ID"), ignore("="))), pattern),                 prune                );
-rule.set(function_definition,  sequence(star(function_argument), match("ID"), ignore("="), function_body ),  toFunctionDefinition );
-rule.set(function_body,        sequence(plus(choice(symbol, expr, operator)), ignore(";")),               prune                );
-rule.set(symbol,               match("ID"),                                                                  toRule("symbol")     );
+rule.set(function_pattern,     sequence(notAt(sequence(match("ID"), ignore("="))), pattern),                prune      );
+rule.set(function_body,        sequence(plus(expr), ignore(";")),                                           prune      );
+rule.set(function_definition,  sequence(star(function_pattern), match("ID"), ignore("="), function_body ),  toFunction );
 
 rule.set(program, sequence(plus(function_definition), ignore("END")), prune );
 
