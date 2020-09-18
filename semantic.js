81791567ceca8e9c1099382dbb3c87423100d291
Pablo Bravo <yandroskaos@gmail.com>
2020-09-18 12:11:26 +0200

    verification no duplicated symbols exists in patterns

--- semantic.js
+++ semantic.js
@@ -1,5 +1,30 @@
 const {astFunctionExists} = require('./ast');
 
+const patternsWalkSymbols = patterns => {
+    return patterns.reduce( (symbols, pattern) => {
+        return symbols.concat(patternGetSymbols(pattern))
+    }, []);
+}
+
+const patternGetSymbols = pattern => {
+    let symbols = []
+
+    if("name" in pattern)
+        symbols.push(pattern.name)
+
+    if (pattern.type == "symbol")
+        symbols.push(pattern.data)
+
+    if (pattern.type == "rest")
+        symbols.push(pattern.data)
+
+    if (pattern.type == "list" || pattern.type == "object")
+        symbols = symbols.concat(patternsWalkSymbols(pattern.data));
+
+    return symbols;
+}
+
+
 const patternsContainSymbol = (patterns, symbol) => {
     return patterns.reduce( (result, pattern) => {
         return result || patternHasSymbol(pattern, symbol)
@@ -43,8 +68,29 @@ const astProcessSymbols = ast => {
     }
 }
 
+const astVerifyFunctionPatterns = ast => {
+    let failure = false
+    for (const fn of ast.fns) {
+        symbols = patternsWalkSymbols(fn.patterns)
+
+        const groupedSymbols = symbols.reduce( (group, symbol) => {
+            group[symbol] = (group[symbol] || 0) + 1;
+            return group;
+        } , {})
+
+        for (const symbol in groupedSymbols) {
+            if(groupedSymbols[symbol] > 1) {
+                console.log(`Symbol "${symbol}" in function "${fn.name}" is defined ${groupedSymbols[symbol]} times in function pattern`)
+                failure = true;
+            }
+        }
+    }
+
+    return !failure;
+}
+
 const astVerifyFunctionBodies = ast => {
-    failure = false
+    let failure = false
     for (const fn of ast.fns) {
         for (const expr of fn.body) {
             if (expr.type == "function" && !ast.internals.includes(expr.data) && !astFunctionExists(ast, expr.data)) {
@@ -63,6 +109,10 @@ const astMainDefined = ast => astFunctionExists(ast, "main")
 function check(ast) {
     //We substitute every symbol in body either as variable or function
     astProcessSymbols(ast);
+
+    //Verify that symbols in patterns are not defined multiple times
+    if(!astVerifyFunctionPatterns(ast)) 
+        return false;
     
     //Verify that every symbol classified as function is indeed defined
     if(!astVerifyFunctionBodies(ast)) 
